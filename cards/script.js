09c273727ed3d4eb899558cc3451ae8f56eb5aa7
ridafatima533 <ffprog12@gmail.com>
2024-11-04 22:21:18 +0500

    Add files via upload

---
+++ cards/script.js
@@ -0,0 +1,4 @@
+function changeColor() {
+    const card = document.querySelector('.card');
+    card.style.backgroundColor = card.style.backgroundColor === 'rgb(52, 152, 219)' ? '#e74c3c' : '#3498db';
+}
